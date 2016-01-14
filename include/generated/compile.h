5aa65f82a354c79b3e9c05a57e9dbfcfa99c524b
jarpii <efren.saenz@hotmail.com>
2016-01-13 21:36:10 -0500

    Iniciando soporte:Huawei Y530

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Mon Jan 11 00:36:19 COT 2016"
+#define LINUX_COMPILE_BY "android"
+#define LINUX_COMPILE_HOST "localhost"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
